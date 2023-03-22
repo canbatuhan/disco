ae4ec67fbd33e974d9162407110a021d2ffc8e80
ituitis-canb18 <canbatuhan.official@gmail.com>
2023-03-22 18:12:32 +0300

    packet definitions

--- include/packet.h
+++ include/packet.h
@@ -3,21 +3,55 @@
 
 #include <stdio.h>
 
+#define CLUSTER_TAG_LENGTH 4
+#define KEY_LENGTH         4
+
+/* Packets - Bytes and Enumerations */
+
+/* Discover Packets */
 #define BORN_BYTE      0x00
 #define GREET_BYTE     0x10
 #define GREET_ACK_BYTE 0x20
 #define HEARTBEAT_BYTE 0x30
 #define DEAD_BYTE      0x40
 
-#define CLUSTER_TAG_LENGTH 2
+/* Request Packets */
+#define SERVE_BYTE 0x50
+#define GET_BYTE   0x60
+#define SET_BYTE   0x70
+
+/* Response Packets */
+#define SERVE_RESP_BYTE 0x80
+#define GET_RESP_BYTE   0x90
+#define SET_RESP_BYTE   0xA0
+
+/* Message Packets */
+#define MESSAGE_BYTE     0xB0
+#define MESSAGE_ACK_BYTE 0xC0
 
 typedef enum packet_types {
-    BORN      = 0,
-    GREET     = 1,
-    GREET_ACK = 2,
-    HEARTBEAT = 3,
-    DEAD      = 4
+    BORN        = 0,
+    GREET       = 1,
+    GREET_ACK   = 2,
+    HEARTBEAT   = 3,
+    DEAD        = 4,
+    SERVE       = 5,
+    GET         = 6,
+    SET         = 7,
+    SERVE_RESP  = 8,
+    GET_RESP    = 9,
+    SET_RESP    = 10,
+    MESSAGE     = 11,
+    MESSAGE_ACK = 12
 } packet_types;
+
+
+/* Encodings - Bytes and Enumerations */
+
+#define UTF_8_BYTE     0x00
+#define UTF_16_BYTE    0x04
+#define ASCII_BYTE     0x08
+#define UNDEFINED_BYTE 0x0C
 
 typedef enum encodings {
     UTF_8     = 0,
@@ -27,7 +61,7 @@ typedef enum encodings {
 } encodings;
 
 
-/* HEADER */
+/* HEADER Byte */
 
 typedef union header {
     unsigned char byte;
@@ -46,10 +80,13 @@ disco_packet_header packet_header(packet_types type, encodings encoding, short r
 
 typedef struct born {
     disco_packet_header header;
-    unsigned char cluster_tag[CLUSTER_TAG_LENGTH];
+    union {
+        unsigned char bytes[CLUSTER_TAG_LENGTH];
+        unsigned cluster_tag : CLUSTER_TAG_LENGTH * 8;
+    } payload;
 } disco_BORN;
 
-disco_BORN born_packet(unsigned char cluster_tag[CLUSTER_TAG_LENGTH]);
+disco_BORN born_packet(unsigned cluster_tag);
 
 
 /* GREET Packets */
@@ -58,13 +95,12 @@ typedef struct greet {
     disco_packet_header header;
 } disco_GREET;
 
-disco_GREET greet_packet(int duplicate);
-
 typedef struct greet_ack {
     disco_packet_header header;
 } disco_GREET_ACK;
 
-disco_GREET_ACK greet_ack_packet(int duplicate);
+disco_GREET greet_packet(short duplicate);
+disco_GREET_ACK greet_ack_packet(short duplicate);
 
 
 /* HEARTBEAT Packet */
@@ -80,10 +116,104 @@ disco_HEARTBEAT heartbeat_packet();
 
 typedef struct dead {
     disco_packet_header header;
-    unsigned char cluster_tag[CLUSTER_TAG_LENGTH];
+    union {
+        unsigned char bytes[CLUSTER_TAG_LENGTH];
+        unsigned cluster_tag : CLUSTER_TAG_LENGTH * 8;
+    } payload;
+    
 } disco_DEAD;
 
-disco_DEAD dead_packet(unsigned char cluster_tag[CLUSTER_TAG_LENGTH]);
+disco_DEAD dead_packet(unsigned cluster_tag);
 
+
+/* SERVE Packets */
+
+typedef struct serve {
+    disco_packet_header header;
+    struct {
+        char* service_name;
+        char* raw_data;
+    } payload;
+} disco_SERVE;
+
+typedef struct serve_resp {
+    disco_packet_header header;
+    struct {
+        char* raw_response;
+    } payload;
+} disco_SERVE_RESP;
+
+disco_SERVE serve_packet(const char* service_name, char* raw_data, encodings encoding, short duplicate);
+disco_SERVE_RESP serve_resp_packet(char* raw_response, encodings encoding, short duplicate);
+
+
+/* GET Packets */
+
+typedef struct get {
+    disco_packet_header header;
+    union {
+        unsigned char bytes[KEY_LENGTH];
+        unsigned key : KEY_LENGTH * 8;
+    } payload;
+} disco_GET;
+
+typedef struct get_resp {
+    disco_packet_header header;
+    struct {
+        char* raw_response;
+    } payload;
+} disco_GET_RESP;
+
+disco_GET get_packet(unsigned key, short duplicate);
+disco_GET_RESP get_resp_packet(char* raw_response, encodings encoding, short duplicate);
+
+
+/* SET Packets */
+
+typedef struct set {
+    disco_packet_header header;
+    struct {
+        unsigned key : KEY_LENGTH * 8;
+        char* raw_data;
+    } payload;
+} disco_SET;
+
+typedef struct set_resp {
+    disco_packet_header header;
+    union {
+        unsigned char byte;
+        struct {
+            unsigned reserved : 7;
+            unsigned success  : 1;
+        } bits;
+    } payload;
+} disco_SET_RESP;
+
+disco_SET set_packet(unsigned key, char* raw_data, encodings encoding, short retain, short duplicate);
+disco_SET_RESP set_resp_packet(unsigned success, short duplicate);
+
+
+/* MESSAGE Packets */
+
+typedef struct message {
+    disco_packet_header header;
+    struct {
+        char* raw_message;
+    } payload;
+} disco_MESSAGE;
+
+typedef struct message_resp {
+    disco_packet_header header;
+    union {
+        unsigned char byte;
+        struct {
+            unsigned reserved : 7;
+            unsigned success  : 1;
+        } bits;
+    } payload;
+} disco_MESSAGE_ACK;
+
+disco_MESSAGE message_packet(char* raw_message, encodings encoding, short retain, short duplicate);
+disco_MESSAGE_ACK message_ack_packet(unsigned success, short duplicate);
 
 #endif
--- src/packet.c
+++ src/packet.c
@@ -1,15 +1,7 @@
+#include <stdlib.h>
 #include <string.h>
 #include "../include/packet.h"
 
-/**
- * @brief Get the packet header object
- * 
- * @param type 
- * @param encoding 
- * @param retain 
- * @param duplicate 
- * @return disco_packet_header 
- */
 disco_packet_header packet_header(packet_types type, encodings encoding, short retain, short duplicate) {
     disco_packet_header header;
     header.bits.type = type;
@@ -19,22 +11,20 @@ disco_packet_header packet_header(packet_types type, encodings encoding, short r
     return header;
 }
 
-disco_BORN born_packet(unsigned char cluster_tag[CLUSTER_TAG_LENGTH]) {
+disco_BORN born_packet(unsigned cluster_tag) {
     disco_BORN packet;
     packet.header = packet_header(BORN, UNDEFINED, 0, 0);
-    for (int idx=0; idx<CLUSTER_TAG_LENGTH; idx++) {
-        packet.cluster_tag[idx] = cluster_tag[idx];
-    }
+    packet.payload.cluster_tag = cluster_tag;
     return packet;
 }
 
-disco_GREET greet_packet(int duplicate) {
+disco_GREET greet_packet(short duplicate) {
     disco_GREET packet;
     packet.header = packet_header(GREET, UNDEFINED, 0, duplicate);
     return packet;
 }
 
-disco_GREET_ACK greet_ack_packet(int duplicate) {
+disco_GREET_ACK greet_ack_packet(short duplicate) {
     disco_GREET_ACK packet;
     packet.header = packet_header(GREET_ACK, UNDEFINED, 0, duplicate);
     return packet;
@@ -46,11 +36,73 @@ disco_HEARTBEAT heartbeat_packet() {
     return packet;
 }
 
-disco_DEAD dead_packet(unsigned char cluster_tag[CLUSTER_TAG_LENGTH]) {
+disco_DEAD dead_packet(unsigned cluster_tag) {
     disco_DEAD packet;
     packet.header = packet_header(DEAD, UNDEFINED, 0, 0);
-    for (int idx=0; idx<CLUSTER_TAG_LENGTH; idx++) {
-        packet.cluster_tag[idx] = cluster_tag[idx];
-    }
+    packet.payload.cluster_tag = cluster_tag;
+    return packet;
+}
+
+disco_SERVE serve_packet(const char* service_name, char* raw_data, encodings encoding, short duplicate) {
+    disco_SERVE packet;
+    packet.header = packet_header(SERVE, encoding, 0, duplicate);
+    packet.payload.service_name = (char*) malloc(strlen(service_name));
+    packet.payload.raw_data = (char*) malloc(strlen(raw_data));
+    strcpy(packet.payload.service_name, service_name);
+    strcpy(packet.payload.raw_data, raw_data);
+    return packet;
+}
+
+disco_SERVE_RESP serve_resp_packet(char* raw_response, encodings encoding, short duplicate) {
+    disco_SERVE_RESP packet;
+    packet.header = packet_header(SERVE_RESP, encoding, 0, duplicate);
+    packet.payload.raw_response = (char*) malloc(strlen(raw_response));
+    strcpy(packet.payload.raw_response, raw_response);
+    return packet;
+}
+
+disco_GET get_packet(unsigned key, short duplicate) {
+    disco_GET packet;
+    packet.header = packet_header(GET, UNDEFINED, 0, duplicate);
+    packet.payload.key = key;
+    return packet;
+}
+
+disco_GET_RESP get_resp_packet(char* raw_response, encodings encoding, short duplicate) {
+    disco_GET_RESP packet;
+    packet.header = packet_header(GET_RESP, encoding, 0, duplicate);
+    packet.payload.raw_response = (char*) malloc(strlen(raw_response));
+    strcpy(packet.payload.raw_response, raw_response);
+    return packet;
+}
+
+disco_SET set_packet(unsigned key, char* raw_data, encodings encoding, short retain, short duplicate) {
+    disco_SET packet;
+    packet.header = packet_header(SET, UNDEFINED, retain, duplicate);
+    packet.payload.key = key;
+    packet.payload.raw_data = (char*) malloc(strlen(raw_data));
+    strcpy(packet.payload.raw_data, raw_data);
+    return packet;
+}
+
+disco_SET_RESP set_resp_packet(unsigned success, short duplicate) {
+    disco_SET_RESP packet;
+    packet.header = packet_header(SET_RESP, UNDEFINED, 0, duplicate);
+    packet.payload.bits.success = success;
+    return packet;
+}
+
+disco_MESSAGE message_packet(char* raw_message, encodings encoding, short retain, short duplicate) {
+    disco_MESSAGE packet;
+    packet.header = packet_header(MESSAGE, UNDEFINED, retain, duplicate);
+    packet.payload.raw_message = (char*) malloc(strlen(raw_message));
+    strcpy(packet.payload.raw_message, raw_message);
+    return packet;
+}
+
+disco_MESSAGE_ACK message_ack_packet(unsigned success, short duplicate) {
+    disco_MESSAGE_ACK packet;
+    packet.header = packet_header(MESSAGE_ACK, UNDEFINED, 0, duplicate);
+    packet.payload.bits.success = success;
     return packet;
 }
